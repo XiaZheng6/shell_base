4b794f80aa8d0d6d6134ec56d4a278237853357e
XiaZheng <hodge_52444@163.com>
2018-01-13 00:16:13 +0800

    Create example-2.sh

---
+++ grep/example-2.sh
@@ -0,0 +1,7 @@
+grep '[0-9]' passwd 
+grep -v '[0-9]' passwd 
+grep -nv '[0-9]' /etc/inittab 
+grep -n '^#' /etc/inittab 
+grep -nv '^#' /etc/nginx/nginx.conf
+grep -nv '^[^a-zA-Z]' inittab 
+grep -n '^[^0-9]' inittab 
