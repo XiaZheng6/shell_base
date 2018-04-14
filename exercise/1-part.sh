b11b85a66762dc47ccaeef7586d565d868e6b541
XiaZheng <hodge_52444@163.com>
2018-04-14 15:05:36 +0800

    Update 1-part.sh

--- exercise/1-part.sh
+++ exercise/1-part.sh
@@ -5,3 +5,22 @@ df -h > $logfile
 
 #n=`wc -l /etc/passwd|awk '{print $1}'`
 #echo $n
+
+date +%T
+#时间
+14:59:22
+
+date -d "-1 day" +%F
+#一天以前
+2018-04-13
+
+ls /etc/passwd /etc/nofile
+ls: cannot access /etc/nofile: No such file or directory
+/etc/passwd
+
+ls /etc/passwd /etc/nofile > /tmp/log 2>/tmp/error
+
+cat /tmp/log 
+/etc/passwd
+cat /tmp/error 
+ls: cannot access /etc/nofile: No such file or directory
