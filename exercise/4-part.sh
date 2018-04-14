d8a38905ea2e49dd606f4179f78c2053800ce82c
XiaZheng <hodge_52444@163.com>
2018-04-14 16:43:26 +0800

    Create 4-part.sh

---
+++ exercise/4-part.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ip=114.114.114.114
+em=abc@139.com
+while 1
+do
+    n=`ping -c 10 114.114.114.114 2>/dev/null |grep 'received'|awk -F 'received, |%' '{ print $2 }'`
+    if [ -z "$n" ]
+    then
+        echo "There is sth wrong in the script."
+        exit
+    fi
+    if [ $n -ge 20 ]
+    then
+        python /usr/bin/local/sbin/mail.py $ma    "$ip down"    "$ip is down"
+    fi
+sleep 30
+done
